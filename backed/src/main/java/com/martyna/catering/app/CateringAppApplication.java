b080c81de4e0f3de1edaa044878b4fb4d9075429
martyna882 <martyna.drabinska@gmail.com>
2019-07-08 21:56:43 +0200

    init repo

---
+++ backed/src/main/java/com/martyna/catering/app/CateringAppApplication.java
@@ -0,0 +1,13 @@
+package com.martyna.catering.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringAppApplication.class, args);
+    }
+
+}
