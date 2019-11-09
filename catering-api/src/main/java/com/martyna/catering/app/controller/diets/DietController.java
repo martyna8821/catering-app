6430b63c2786fbf65fbd5f6ff6701e8c9eed3c9d
martyna8821 <martyna.drabinska@gmail.com>
2019-11-09 15:16:59 +0100

    get all diets

--- catering-api/src/main/java/com/martyna/catering/app/controller/diets/DietController.java
+++ catering-api/src/main/java/com/martyna/catering/app/controller/diets/DietController.java
@@ -7,10 +7,7 @@ import com.martyna.catering.app.service.diets.IDietService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("diet")
@@ -33,4 +30,10 @@ public class DietController {
        // )
         return null;
     }
+
+    @GetMapping("")
+    ResponseEntity<?> getAllDiets(){
+        return null;
+    }
+
 }
