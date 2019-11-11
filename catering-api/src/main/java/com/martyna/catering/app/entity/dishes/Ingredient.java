57e08cdb758155ca528733d9c1c9a43109f509da
martyna8821 <martyna.drabinska@gmail.com>
2019-11-12 00:12:26 +0100

    ingredient type adjusments

--- catering-api/src/main/java/com/martyna/catering/app/entity/dishes/Ingredient.java
+++ catering-api/src/main/java/com/martyna/catering/app/entity/dishes/Ingredient.java
@@ -1,6 +1,7 @@
 package com.martyna.catering.app.entity.dishes;
 
 
+import com.martyna.catering.app.entity.users.Role;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,4 +34,20 @@ public class Ingredient {
     @CollectionTable(name = "ingredient_allergens", joinColumns = @JoinColumn(name = "ingredient_id"))
     @Column(name = "allergens")
     Set<String> allergens = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ingredient_brands", joinColumns = @JoinColumn(name = "ingredient_id"))
+    @Column(name = "brands")
+    Set<String> brands = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ingredient_labels", joinColumns = @JoinColumn(name = "ingredient_id"))
+    @Column(name = "labels")
+    Set<String> labels = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "ingredient_ingredient-types",
+            joinColumns = @JoinColumn(name = "ingredient_type_id"),
+            inverseJoinColumns = @JoinColumn(name = "ingredient_type_id"))
+    private Set<Role> types = new HashSet<>();
 }
