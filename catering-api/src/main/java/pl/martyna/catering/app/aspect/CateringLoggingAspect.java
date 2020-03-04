0ce85130201743dd4376217865b52e75b02a3dd3
martyna8821 <martyna.drabinska@gmail.com>
2020-03-04 20:19:50 +0100

    logging request handling with AOP

--- catering-api/src/main/java/pl/martyna/catering/app/aspect/CateringLoggingAspect.java
+++ catering-api/src/main/java/pl/martyna/catering/app/aspect/CateringLoggingAspect.java
@@ -2,8 +2,10 @@ package pl.martyna.catering.app.aspect;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +15,9 @@ public class CateringLoggingAspect {
 
     private final Logger LOG = LogManager.getLogger(getClass());
 
-    @Before("execution(public * getAllDiets())")
-    public void beforeGetAllDietsAdvice(){
-        LOG.info("Executing getAllDiets() method");
+    @Before("execution(* pl.martyna.catering.app.controller.*.*.*(..)))")
+    public void beforeRequestHandling(JoinPoint joinPoint){
+        LOG.info("Handling request: " + joinPoint.getSignature().getName());
     }
 }
+
