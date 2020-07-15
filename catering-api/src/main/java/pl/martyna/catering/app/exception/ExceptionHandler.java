b15091998b34346412d7e3ee0d9fb16c0b110989
martyna8821 <martyna.drabinska@gmail.com>
2020-07-15 19:39:06 +0200

    validation exception

--- catering-api/src/main/java/pl/martyna/catering/app/exception/ExceptionHandler.java
+++ catering-api/src/main/java/pl/martyna/catering/app/exception/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package pl.martyna.catering.app.exception;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -34,20 +35,5 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, ex.getMessage(),
                 new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {
-            MethodArgumentNotValidException.class
-    })
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 }
