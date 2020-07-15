915e3c53bc275d974a36c3d5b15987950a6ebe06
martyna8821 <martyna.drabinska@gmail.com>
2020-07-15 19:06:43 +0200

    some user register validation

--- catering-api/src/main/java/pl/martyna/catering/app/dto/auth/RegisterRequest.java
+++ catering-api/src/main/java/pl/martyna/catering/app/dto/auth/RegisterRequest.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Getter @Setter
@@ -13,11 +16,18 @@ import java.util.Set;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotNull
     private String username;
+    @Size(min=7)
     private String password;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
+    @Pattern(regexp = "^.+@.+\\..+$")
     private String email;
+    @NotNull
     private String phoneNumber;
     private Address address;
     private Set<String> roles;
--- catering-api/src/main/java/pl/martyna/catering/app/exception/ExceptionHandler.java
+++ catering-api/src/main/java/pl/martyna/catering/app/exception/ExceptionHandler.java
@@ -3,11 +3,16 @@ package pl.martyna.catering.app.exception;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
@@ -29,5 +34,20 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, ex.getMessage(),
                 new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = {
+            MethodArgumentNotValidException.class
+    })
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
